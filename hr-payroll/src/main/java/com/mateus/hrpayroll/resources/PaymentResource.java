c69cdedf7d7a5526c244da085f53c33c5ed852b9
lordradamanthys <mateuslima565@gmail.com>
2021-07-10 00:06:07 -0300

    resilience4j alternative for hystrix

--- hr-payroll/src/main/java/com/mateus/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/mateus/hrpayroll/resources/PaymentResource.java
@@ -1,6 +1,7 @@
 package com.mateus.hrpayroll.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,15 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mateus.hrpayroll.entities.Payment;
 import com.mateus.hrpayroll.service.PaymentService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RequestMapping("/payments")
 @RestController
 public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 
+	
 	@GetMapping(value = "/{workerId}/days/{days}")
+	@CircuitBreaker(name = "Payment", fallbackMethod = "error")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
+
+	
+	public ResponseEntity<String> error(Exception e) {
+		
+		return new ResponseEntity<String>("Error teste",HttpStatus.BAD_REQUEST);
+	}
+	
 }
