8390e35c2969d2514ecc2c568adb240551f427f4
lordradamanthys <mateuslima565@gmail.com>
2021-07-12 13:52:09 -0300

    created gateway with zuul proxy

---
+++ hr-api-gateway-1/src/test/java/com/mateus/hrapigateway/HrApiGateway1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.hrapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGateway1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- hr-api-gateway-zuul/src/test/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
+++ hr-api-gateway-zuul/src/test/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -1,10 +1,10 @@
-package com.mateus.hrapigateway;
+package com.devsuperior.hrapigatewayzuul;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HrApiGatewayApplicationTests {
+class HrApiGatewayZuulApplicationTests {
 
 	@Test
 	void contextLoads() {
